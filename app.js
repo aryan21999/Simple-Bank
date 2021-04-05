7cbae8300d8bb2ac4122bfc0bdc3f07180798cff
Aryan <test@test.com>
2021-04-05 12:00:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var redirect = require('express-redirect');
+//mongoose.Promise = global.Promise;
+
+var index = require('./routes/index');
+var auth = require('./routes/auth');
+var register = require('./routes/register');
+var member = require('./routes/member');
+var api = require('./routes/api');
+var app = express();
+redirect(app);
+//Connect to Mongoosemongoose.Promise = global.Promise;
+
+//mongoose.connect('mongodb+srv://Aryan:09876Arya@cluster0.h0l2c.mongodb.net/bank?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://Aryan:09876Arya@cluster0.h0l2c.mongodb.net/Bank?retryWrites=true&w=majority');
+ var db = mongoose.connection;
+
+// var promise = mongoose.connect('mongodb://Aryan:09876Arya@cluster0.h0l2c.mongodb.net/Bank?retryWrites=true&w=majority', {
+//   useMongoClient: true,
+// });
+
+// promise.then(function(db) {
+//     console.log("Connected to database!!!");
+// }, function(err){
+//     console.log("Error in connecting database " + err);
+// });
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/login',auth);
+app.use('/register',register);
+app.use('/member',member);
+app.use('/api',api);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+var port = process.env.PORT || '4000'
+app.listen(port, err => {
+    if (err)
+        throw err
+    console.log('Server listening on port', port)
+})
+
+
+
+module.exports = app;
---
+++ routes/member.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var mongoose = require('mongoose');
+var userdata;
+
+/* Get member page. */
+router.get('/', function(req, res, next) {
+  if(req.cookies.logged){
+    var uid = req.cookies.logged;
+    var query = User.findOne({ '_id' : uid });
+    query.select('name email money card image_url');
+    query.exec(function (err, user) {
+    if (err) return handleError(err);
+      userdata=user;
+      res.render('member', { userdata: user, title: 'member', action:'MAIN' });
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+router.get('/withdraw', function(req, res, next) {
+  res.render('transact', { action: 'WITHDRAW', userdata: userdata, title: 'Withdraw' });
+});
+router.get('/deposit', function(req, res, next) {
+  res.render('member', { action: 'DEPOSIT', userdata: userdata, title: 'Deposit' });
+});
+router.get('/logout', function(req, res, next) {
+  res.clearCookie("logged");
+  res.redirect('/login');
+});
+router.get('/transact', function(req, res, next) {
+  res.send('it goes to get');
+});
+router.post('/transact', function(req, res, next) {
+  console.log('transact working');
+  var amount = req.body.amount;
+  var card = req.body.card;
+  var action = req.body.action;
+  console.log(amount+' '+card+' '+action);
+    if(action=='DEPOSIT'){
+      var newAmount = (parseInt(amount) + parseInt(userdata.money)).toString();
+      var deposit = User.updateOne(
+      { 'card' : card },
+      { $set: { 'money' : newAmount } }
+      );
+      deposit.exec(function (err, result) {
+      if (err) return handleError(err);
+        console.log(result);
+      });
+    };
+    if(action=='WITHDRAW'){
+      var newAmount = parseInt(userdata.money) - parseInt(amount);
+      if(newAmount<0){res.redirect('/member'); return;}
+      var withdraw = User.updateOne(
+      { 'card' : card },
+      { $set: { 'money' : newAmount } }
+      );
+      withdraw.exec(function (err, result) {
+      if (err) return handleError(err);
+        console.log(result);
+      });
+    };
+    res.redirect('/member');
+});
+module.exports = router;
