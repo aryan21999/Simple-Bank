9d249ad11abdc4b5305e5c7395ee80f3e40417ca
Aryan <test@test.com>
2021-04-06 12:25:46 +0530

    make it better

--- controllers/upload.js
+++ controllers/upload.js
@@ -8,8 +8,9 @@ const uploadFile = async (req, res) => {
     if (req.file == undefined) {
       return res.send(`You must select a file.`);
     }
+    res.render('login', { title: 'login' });
+    // return res.send(`File has been uploaded.`);
 
-    return res.send(`File has been uploaded.`);
   } catch (error) {
     console.log(error);
     return res.send(`Error when trying upload image: ${error}`);
